3f04499dabd678a6c50f3ecc3ebc6c47853fd769
Mateo Galache Guillot <mateo.galache@students.salle.url.edu>
2022-10-20 19:18:01 +0200

    login vue 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Inicio from '../views/Inicio.vue'
+import Login from '../views/Login.vue'
 import Contact from '../views/Contact.vue'
 const routes = [
     {
         path: '/',
         name: 'Inicio',
         component: Inicio
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
     },
     
 
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -11,36 +11,5 @@ import Footer from "../components/Footer.vue";
     </main>
 </template>
 
-.HContainer{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    background-color: cadetblue;
-}
-.FContainer{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    background-color: cadetblue;
-    align-items: center;
-    flex-wrap: wrap;
-    
-}
-.Fright{
-    display: flex;
-    justify-content: space-around;
-    flex-grow: 1;
-    padding-left: 8%;
-    
-    
-    
-}
-
-.Fleft{
-    display: flex;
-    justify-items: center;
-    padding-right: 15%;
-    padding-left: 10%;
-}
-
+<style>
 </style>
