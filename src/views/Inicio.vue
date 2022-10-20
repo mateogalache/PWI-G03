35db17dd6339c90b945b21066edecf818014c080
Mario Rodriguez Alvarez <mario.ra@students.salle.url.edu>
2022-10-20 19:21:31 +0200

    Config

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,14 +1,13 @@
-
 <script setup>
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 </script>
 <template>
+    <Header></Header>
     <main>
-        <Header></Header>
-        <Footer></Footer>
-        
+        <h1>COSASASAS</h1>
     </main>
+    <Footer></Footer>
 </template>
 
 <style>
