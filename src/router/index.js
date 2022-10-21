c744db7c586fba6404a9bc9f91bee2883204799f
Mateo Galache Guillot <mateo.galache@students.salle.url.edu>
2022-10-21 16:39:39 +0200

    perfil + borrar cuenta vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Inicio from '../views/Inicio.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import Perfil from '../views/Perfil.vue'
+import Borrarperfil from '../views/Borrarperfil.vue'
 const routes = [
     
     {
@@ -13,6 +15,16 @@ const routes = [
         path: '/Register',
         name: 'Register',
         component: Register
+    },
+    {
+        path: '/Perfil',
+        name: 'Perfil',
+        component: Perfil
+    },
+    {
+        path: '/Borrarperfil',
+        name: 'Borrarperfil',
+        component: Borrarperfil
     },
 
 ]
