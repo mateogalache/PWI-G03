ffa56cf0c60a5540ec26f3e0af898b2a195644d2
socratin98 <socratesdelae@gmail.com>
2022-10-27 18:22:05 +0200

    HTML de add(crearEvento)

---
+++ src/views/Add.vue
@@ -0,0 +1,51 @@
+<script setup>
+import Footer2 from '../components/Footer2.vue'
+import Header3 from '../components/Header3.vue'
+</script>
+
+<template>
+
+    <Header3>
+            
+    </Header3>
+
+<div class = "container">
+
+    <div class = "subtitulo">           
+            <p style="color: black">Creación Evento</p>
+    </div>
+
+    <div class = "rectanguloredondeadeo">           
+            <p style="color: grey">Nombre Evento</p>
+    </div>
+
+    <div class = "rectanguloredondeadeo">           
+            <p style="color: grey">Tipo Evento</p>
+    </div>
+
+    <div class = "rectanguloredondeadeo">           
+            <p style="color: grey">Dia Evento</p>
+    </div>
+
+    <div class = "rectanguloredondeadeo">           
+            <p style="color: grey">Localización</p>
+    </div>
+
+    <div class = "rectanguloredondeadeo">           
+            <p style="color: grey">Descripción</p>
+    </div>
+
+    <div class = "reiniciar">           
+            <p style="color: black">Reiniciar</p>
+    </div>
+
+    <div class = "crear">           
+            <p style="color: black">Crear</p>
+    </div>
+</div>
+
+    <Footer2>
+        
+    </Footer2>
+    
+</template>
