37cb02155aa9993e485628743314f6af5d8ec4bd
Mateo Galache Guillot <mateo.galache@students.salle.url.edu>
2022-10-28 11:32:04 +0200

    log in dekstop

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,27 +7,36 @@ import Header from '../components/Header.vue'
         
     </Header>
     <main>
-        <br><br/><br/>
-        <div class="cont">        
+        <br><br/>
+        <div class="contlogin">        
         <img src = "https://i.ibb.co/80sWyBS/sallevents.png" width = "200" height = "100" >
         </div>
+
         <br>
-        <h2><b>Iniciar sesión</b></h2>              
-        <p><label>Email*</label></p>
-        <input type="text" class = "texto">
-        <p><label>Contraseña*</label></p>
-        <input type="text" class = "texto">
-       <p><small>¿Has olvidado tu contraseña?</small></p>
+        <div class="contlogin">
+        <div class = "center0">
+            <h2><b>Iniciar sesión</b></h2>              
+            <p><label>Email*</label></p>
+            <input type="text" class = "texto">
+            <p><label>Contraseña*</label></p>
+            <input type="text" class = "texto">
+            <p><small>¿Has olvidado tu contraseña?</small></p>
+        </div>
+       </div>
        <br>
     
-        <div class="cont">
+       <div class="contlogin"> 
             <a href = "Home"><button class="Iniciar">
             <b><FONT COLOR="white">Iniciar Sesión</FONT></b>
-        </button></a>
+            </button></a>
         </div>
-        <a HREF="Register"><p align = "right"><small>¿No tienes cuenta? Regístrate</small></p></a>
+        <div class="contlogin">
+        <div class = "center0">
+         <a HREF="Register"><p align = "right"><small>¿No tienes cuenta? Regístrate</small></p></a>
+        </div>
+       </div>
+        
        
-
         
     </main>
     <Footer>
@@ -36,6 +45,11 @@ import Header from '../components/Header.vue'
   </template>
 <style>
 
+.center0{
+   display: block;
+   width: 100%;
+   
+}
 .Iniciar{
    display: flex;
    width:150px;
@@ -48,21 +62,7 @@ import Header from '../components/Header.vue'
 
    
 } 
-.HContainer{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    background-color: cadetblue;
-}
-.FContainer{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    background-color: cadetblue;
-    align-items: center;
-    flex-wrap: wrap;
-    
-}
+
  .texto{
     width: 98%;
     background-color: transparent;
@@ -70,8 +70,26 @@ import Header from '../components/Header.vue'
     border-bottom: 1px solid black;
 
   }   
+
+  .contlogin{
+    display: flex;
+    justify-content: center;
+   align-items: center;
+   
+
+}
+    
+@media (min-width: 850px){
+    .center0{
+        width: 500px;
+        
+    }
+    
+
+
     
     
+}
 
 
 
