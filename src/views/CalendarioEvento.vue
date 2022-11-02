6c6abaf590abd8d439063795cbdf63c8e3a0b1a8
socratin98 <socratesdelae@gmail.com>
2022-11-02 12:36:14 +0100

    CalendarioEvento

--- src/views/CalendarioEvento.vue
+++ src/views/CalendarioEvento.vue
@@ -1,73 +1,170 @@
 <script setup>
 import Footer2 from '../components/Footer2.vue'
-import Header3 from '../components/Header3.vue';
 
+import Header3 from '../components/Header3.vue'
 </script>
 
 
 <template>
-    
-    <Header3>
-            
-    </Header3>
 
+<div class="rectanguloopacidad">
+
+</div>
+<Header3>
+        
+</Header3>
+
+<main>
     <div class = "container">
         <div class = "rectanguloredondeadeo2">           
-            Buscar evento...
+            Buscar Evento
         </div>
         <div class = "rectanguloredondeadeo3">           
             Filtro
         </div>
-        <div class="fondo">
-            <div class="popup"> 
-                <div class="popup2">
-                    Lugar...
-                </div>
-                <div class="popup2">
-                    Fecha Inicio...
-                    <div class="calendario">
-                        <img src = "src/assets/calendar.png"
-                        width="15" height="15" alt="Botón calendario">
-                    </div>
-                </div>
-                <div class="popup3">
-                    <b>Aplicar</b>
-                </div>
-                <div class="popup4">
-                    <b>Cancelar</b>
-                </div>
+        <div class = "rectanguloredondeadeo4">           
+            Historial
+        </div>
+        <div class="eventocontainer">
+
+            <div class = "parejas">
+            <a href="Event"> <img src="src/assets/ImagenesEventos/deporte.png" 
+                width="35" height="35" alt="Botón atrás"></a>
+                <img src="src/assets/ImagenesEventos/vino.png" 
+                width="35" height="35" alt="Botón atrás">
             </div>
+
+            <div class = "parejas">
+                <img src="src/assets/ImagenesEventos/teatro.png" 
+                width="35" height="35" alt="Botón atrás">
+                <img src="src/assets/ImagenesEventos/libro.png" 
+                width="35" height="35" alt="Botón atrás">
+            </div>
+
+            <div class = "parejas">
+                <img src="src/assets/ImagenesEventos/comida.png" 
+                width="35" height="35" alt="Botón atrás">
+                <img src="src/assets/ImagenesEventos/viajes.png" 
+                width="35" height="35" alt="Botón atrás">
+            </div>
+            
         </div>
         
+        <div class="popup"> 
+                <div class="popup2">
+                    <p>Lugar...</p>
+                </div>
+                <div class="popup2">
+                    <p>Fecha Inicio...</p>
+                        <div class="imga">
+                            <img src = "src/assets/calendar.png">
+                        </div>
+                </div>
+                <div class="botones2">
+                    <div class="popup3">
+                        <b>Aplicar</b>
+                    </div>
+                    <div class="popup3">
+                        <b>Cancelar</b>
+                    </div>
+                </div>
+                
+            </div>
+
     </div>
 
-    <Footer2>
-            
-    </Footer2>
+</main>
+
+        
+<Footer2>
+        
+</Footer2>
+
+<div class="rectanguloopacidad">
+    
+</div>
 
 </template>
 
-
 <style scoped>
+
     .container{
-        height: 472px;
-        margin: 7em;
+       
         display: flex;
         justify-content: center;
         flex-direction: column;
         align-items: center;
-        background-color: rgba(0, 0, 0, 0.6);
     }
 
-    .fondo{
+    .eventocontainer{
         background-color: lightgrey;
-        height: 500px;
-        width: 300px;
-        padding: 2em;
-        margin-top: 1px;
-        justify-content: center;
-        align-items: center;
         display: flex;
+        flex-direction: column;
+        padding: 2em;
+        margin-top: 20px;
+    }
+
+    img{
+        width: 120px;
+        height: 120px;
+        margin: 1em;
+    }
+
+    .parejas{
+        display: flex;
+        flex-direction: row;
+    }
+
+    .rectanguloredondeadeo2{
+    color: gray;
+    height: 25px;
+    width: 275px;
+    background-color: transparent;
+    border: 1px solid grey;
+    display: flex;
+    align-items:center;
+    padding: 5px;
+    border-radius: 50px;
+    margin: 25px;
+    margin-right: 100px;
+    }
+
+    .rectanguloredondeadeo3{
+    color: white;
+    height: 25px;
+    width: 75px;
+    background-color: grey;
+    border: 1px solid grey;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    border-radius: 50px;
+    margin: 25px;
+    margin-right: -275px;
+    margin-top: -57px;
+    }
+
+    .rectanguloredondeadeo4{
+    color: gray;
+    height: 50px;
+    width: 275px;
+    background-color: transparent;
+    border: 1px solid grey;
+    display: flex;
+    align-items:top;
+    padding: 5px;
+    border-radius: 5px;
+    margin: 25px;
+    margin-right: 100px;
+    margin-top: -10px;
+    }
+
+    .rectanguloopacidad{
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      background-color: black;
+      opacity: 70%;
     }
 
     .popup{
@@ -80,6 +177,7 @@ import Header3 from '../components/Header3.vue';
         align-items: top;
         display: flex;
         flex-direction: column;
+        position: absolute;
     }
 
     .popup2{
@@ -90,69 +188,33 @@ import Header3 from '../components/Header3.vue';
         border: 1px solid grey;
         border-radius: 1px;
         margin-top: 10px;
-        justify-content: left;
-        align-items: top;
+        align-items: center;
         display: flex;
+        justify-content: space-between;
     }
 
     .popup3{
-        color: gray;
+        color: black;
         height: 25px;
         width: 75px;
         background-color: transparent;
         border: 1px solid grey;
-        align-items:top;
         border-radius: 8px;
         margin-top: 10px;
-        text-align: center;
-        float: left;
-
-    }
-
-    .popup4{
-        color: gray;
-        height: 25px;
-        width: 75px;
-        background-color: transparent;
-        border: 1px solid grey;
-        align-items:top;
-        border-radius: 8px;
-        margin-top: 10px;
-        text-align: center;
-        float: left;
-    }
-
-
-    .rectanguloredondeadeo2{
         display: flex;
-        color: gray;
-        height: 25px;
-        width: 350px;
-        background-color: transparent;
-        border: 1px solid grey;
-        align-items:center;
-        padding: 3px;
-        border-radius: 50px;
-        margin-top: -70px;
-        margin-left: -20px;
-    }
-
-    .rectanguloredondeadeo3{
-        color: white;
-        height: 25px;
-        width: 75px;
-        background-color: grey;
-        border: 1px solid grey;
-        display: flex;
-        align-items:center;
         justify-content: center;
-        border-radius: 50px;
-        margin: 15px;
-        margin-left: 275px;
+        align-items: center;
     }
 
-    .calendario{
-        margin-left: 80px;
+	.imga{
+        height: 1%;
+        width: 1%;
     }
+
+    .botones2{
+        display: flex;
+        justify-content: space-around;
+    }
+
 
 </style>
