ebb262fde03a114c7c842398ebadaf3abfdd6b83
Socrates98 <socratesdelae@gmail.com>
2022-11-02 10:36:48 +0100

    id'sContactanos

--- src/views/Contactanos.vue
+++ src/views/Contactanos.vue
@@ -10,58 +10,60 @@ import Header3 from '../components/Header3.vue';
     <Header3>
             
     </Header3>
+    <main>
+        <div id="container1">
+            <div id="eventocontainer1">
 
-    <div class="container1">
-        <div class="eventocontainer1">
+                <h1 id = "titulo">Contacta con nuestro equipo</h1>
+                <div id = textoo aling = "center">
+                    Nos encantan las preguntas y los comentarios <br>
+                    y siempre estamos dispuestos a ayudar.<br><br>
 
-            <h1 class = "titulo">Contacta con nuestro equipo</h1>
-            <div class = textoo aling = "center">
-                Nos encantan las preguntas y los comentarios <br>
-                y siempre estamos dispuestos a ayudar.<br><br>
-
-                Aquí tienes algunas opciones para ponerte<br>
-                en contacto con nosotros
-            </div>
-
-            <div class="popup">
-                <h2 class = "textoo">Reportar<br> un problema</h2>
-                <div class = textoo2>
-                    Deje aquí su sugerencia o problema que ha tenido con la apliación.
-                    Le contacremos lo más rápido posible.
+                    Aquí tienes algunas opciones para ponerte<br>
+                    en contacto con nosotros
                 </div>
-                <div class = "botones">           
-                    Reportar
-                </div>
-            </div>
 
-            <div class="popup">
-                <h2 class = "textoo">Contactar con el servicio de atención al cliente</h2>
-                <div class = textoo3>
-                    Estamos esperando para ayudarle a usted y a su equipo.
-                </div>
-                <div class = "botones">           
-                    Contactar
-                </div>
-            </div>
-
-            <div class="container2">
-                <div class = "parejas">
-                    <div>
-                        <b>SallEvents</b><br>
+                <div id ="popup">
+                    <h2 id = "textoo">Reportar<br> un problema</h2>
+                    <div id = textoo2>
+                        Deje aquí su sugerencia o problema que ha tenido con la apliación.
+                        Le contacremos lo más rápido posible.
                     </div>
-                    <div>
-                        Diagonal, 108, 5to 2nd
+                    <div id = "botones">           
+                        Reportar
                     </div>
-                    <div>
-                        <img src="src/assets/ImagenesEventos/redes_sociales.png" 
-                        width="100" height="20" alt="RedesSociales">
+                </div>
+
+                <div id="popup">
+                    <h2 id = "textoo">Contactar con el servicio de atención al cliente</h2>
+                    <div id = textoo3>
+                        Estamos esperando para ayudarle a usted y a su equipo.
                     </div>
-                    <img src="src/assets/ImagenesEventos/mapa.png" 
-                    width="200" height="200" alt="Mapa">
+                    <div id = "botones">           
+                        Contactar
+                    </div>
+                </div>
+
+                <div id="container2">
+                    <div id = "parejas">
+                        <div>
+                            <b>SallEvents</b><br>
+                        </div>
+                        <div>
+                            Diagonal, 108, 5to 2nd
+                        </div>
+                        <div>
+                            <img id="img" src="src/assets/ImagenesEventos/redes_sociales.png" 
+                            width="100" height="20" alt="RedesSociales">
+                        </div>
+                        <img id="img" src="src/assets/ImagenesEventos/mapa.png" 
+                        width="200" height="200" alt="Mapa">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
+    
     
     <Footer2>
     </Footer2>
@@ -69,20 +71,41 @@ import Header3 from '../components/Header3.vue';
 </template>
 
 <style scoped>
-    .titulo{
+
+    #container1{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-right: 2%;
+    }
+
+    #eventocontainer1{
+        background-color: lightgrey;
+        display: flex;
+        flex-direction: column;
+        margin-top: 10px;
+        justify-content: center;
+        width: 95%;
+        height: 95%;
+        align-items: center;
+    }
+
+    #titulo{
         display: flex;
         font-size: large;
         justify-content: center;
     }
 
-    .textoo{
+    #textoo{
         display: flex;
         font-size: small;
         justify-content: center;
         text-align: center;
     }
 
-    .textoo2{
+    #textoo2{
         display: flex;
         font-size: x-small;
         justify-content: center;
@@ -90,7 +113,7 @@ import Header3 from '../components/Header3.vue';
         margin: 10px;
     }
 
-    .textoo3{
+    #textoo3{
         display: flex;
         font-size: x-small;
         justify-content: center;
@@ -98,24 +121,7 @@ import Header3 from '../components/Header3.vue';
         margin: 15px;
     }
 
-    .container1{
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        align-items: center;
-    }
-
-    .eventocontainer1{
-        background-color: lightgrey;
-        display: flex;
-        flex-direction: column;
-        padding: 1em;
-        margin-top: 40px;
-        width: 350px;
-        align-items: center;
-    }
-
-    .popup{
+    #popup{
         background-color: white;
         height: 125px;
         width: 200px;
@@ -124,7 +130,7 @@ import Header3 from '../components/Header3.vue';
         border: 1px solid grey;
     }
 
-    .botones{
+    #botones{
         height: 12px;
         width: 68px;
         background-color: grey;
@@ -140,7 +146,7 @@ import Header3 from '../components/Header3.vue';
         margin: 0 auto;
     }
 
-    .container2{
+    #container2{
 
         display: flex;
         justify-content: center;
@@ -148,9 +154,14 @@ import Header3 from '../components/Header3.vue';
         align-items: center;
     }
 
-    .parejas{
+    #parejas{
         display: flex;
         flex-direction: row;
+    }
+
+    #img{
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
     }
 
  
