1484e68045fa862646cea977665217bb47e484c9
socratin98 <socratesdelae@gmail.com>
2022-11-08 17:45:59 +0100

    CompartirEventoAmigos(Desktop)

--- src/views/CompartirEventoAmigos.vue
+++ src/views/CompartirEventoAmigos.vue
@@ -16,33 +16,33 @@ import Header3 from '../components/Header3.vue'
                     <h1>Amistades</h1>
 
                         <ul>
-                        <li>Más interacción:
+                        <li class="subtitulos">Más interacción:
                             <br><br>
                             <ol>
-                                <li><button class="btn success">José Manuel</button></li>
-                                <li><button class="btn success">Carlos</button></li>
-                                <li><button class="btn success">Javier</button></li>
-                                <li><button class="btn success">Sandra</button></li>  
+                                <li><button>José Manuel</button></li>
+                                <li><button>Carlos</button></li>
+                                <li><button>Javier</button></li>
+                                <li><button>Sandra</button></li>  
                             </ol>
                         </li>
                         <br>
                         <li>Más olvidados:
                             <br><br>
                             <ol>
-                                <li><button class="btn success">Fran</button></li>
-                                <li><button class="btn success">Carla</button> </li>
-                                <li><button class="btn success">Ana</button></li>
-                                <li><button class="btn success">Sergio</button></li>
+                                <li><button>Fran</button></li>
+                                <li><button>Carla</button> </li>
+                                <li><button>Ana</button></li>
+                                <li><button>Sergio</button></li>
                             </ol>
                         </li>
                         <br>
                         <li>Favoritos:
                             <br><br>
                             <ol>
-                                <li><button class="btn success">Mateo</button></li>
-                                <li><button class="btn success">Sócrates</button> </li>
-                                <li><button class="btn success">Mario</button></li>
-                                <li><button class="btn success">Fran</button></li>
+                                <li><button>Mateo</button></li>
+                                <li><button>Sócrates</button> </li>
+                                <li><button>Mario</button></li>
+                                <li><button>Fran</button></li>
                             </ol>
                         </li>
                         </ul>
@@ -94,6 +94,41 @@ import Header3 from '../components/Header3.vue'
         display: flex;
         border-radius: 50px;
         margin: 0 auto;
+    }
+
+    @media (min-width:1080px){
+        .containercea{
+            display: flex;
+            text-align: center;
+            padding: 1em;
+        }
+
+        .eventocontainercea{
+            background-color: lightgrey;
+            display: flex;
+            flex-direction: column;
+            padding: 1em;
+            text-align: left    ;
+            margin-top: -6px;
+            width: 95%;        
+        }
+
+        .botonenviar{
+            height: 40px;
+            width: 110px;
+            background-color: white;
+            border: 1px solid grey;
+            align-items:center;
+            justify-content: center;
+            padding: 1px;
+            display: flex;
+            border-radius: 50px;
+            margin: 0 auto;
+        }
+
+        .subtitulos{
+            margin: 0 auto;
+        }
     }
 
 </style>
