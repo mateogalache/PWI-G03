048e28df2728ec9021f13dcdc7301822eca7e321
Francisco Bellver Asperilla <francisco.bellver@students.salle.url.edu>
2023-01-04 14:09:22 +0100

    calendario

--- src/views/TimeLineEvent.vue
+++ src/views/TimeLineEvent.vue
@@ -1,3 +1,63 @@
+<script>
+const token = localStorage.getItem('accessToken');
+const event_id = window.localStorage.getItem('event');
+const id = localStorage.getItem('userId');
+export default {
+  name: "App",
+  data() {
+    return {
+      data: {},
+      data2: {},
+      editing: false,
+      
+      showPart: false,
+      response: null,
+      participating: false,
+      location: '',
+      npart: '',
+      type:'',
+      name:'',
+      endDate: '',
+      startDate:'',
+      description: '',
+      image: '',
+    }
+  },
+  beforeMount(){
+    
+    this.getEvents();
+    
+  },
+
+methods: {
+getEvents(){     
+      
+      const response = fetch (`http://puigmal.salle.url.edu/api/v2/users/${id}/assistances/future`,{
+        method: 'GET',
+          headers: {
+              "Content-Type": "application/json",
+              'Authorization': `Bearer ${token}`
+          },
+      })
+          
+          .then(response => response.json())
+          .then(data => {
+                    data.sort((a,b) =>{
+                        const dateA = new Date(a.eventStart_date);
+                        const dateB = new Date(b.eventStart_date);
+                        return dateA - dateB;
+                    });
+                    this.data = data;
+                });
+
+          console.log(response);                 
+          console.log(event_id);
+          
+         
+  },
+}
+};
+</script>
 <script setup>
 import Footer2 from '../components/Footer2.vue'
 import Header2 from '../components/Header2.vue'
@@ -9,11 +69,14 @@ import Header2 from '../components/Header2.vue'
         
     </Header2>
 <!--Pagina para ver el calendario de eventos-->
-    <main>
-        <div class = "cont">
-            <h2>Calendar</h2>
-            <img src="src/assets/calendario.jpeg" class = "calendar">
-            
+    <main >
+        <h1>Calendario</h1>
+        <div v-for= "event in data" id="timeline-container">
+            <ul class="timeline">
+                <h2  class="heading">{{event.eventStart_date.substring(0,10)}}</h2>
+                <li  class="timeline-item" >{{event.name}}</li>
+                
+            </ul>
         </div>
     </main>
     <Footer2>
@@ -22,6 +85,59 @@ import Header2 from '../components/Header2.vue'
 </template>
 
 <style scoped>
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+/* TIMELINE */
+#timeline-container {
+  background: white;
+  background: linear-gradient(
+    171deg,
+    
+    
+  );
+  overflow: auto;
+  padding: 60px 0;
+
+  
+    
+  
+}
+
+.timeline {
+  margin: 0 auto;
+  position: relative;
+  left: 120px;
+  width: 80%;
+  max-width: 900px;
+  margin-top: 16px;
+  margin-left: 5%;
+}
+
+.timeline-item {
+  color: black;
+  font-family: var(--font-family-2);
+  text-align: center;
+  font-size: 1.4rem;
+  padding: 1rem 1.5rem 1rem 1.5rem;
+  border-left: 4px solid black;
+  border-bottom: 1px solid black;
+  position: relative;
+  list-style-type: none;
+  --item-width: calc(100%);
+}
+  
+
+  
+ 
+
 
 .cont{
 
