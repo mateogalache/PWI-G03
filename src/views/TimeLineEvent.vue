1c0b572ba041e521ac3a181650ec2e28770a1837
Francisco Bellver Asperilla <francisco.bellver@students.salle.url.edu>
2023-01-21 13:17:31 +0100

    share event + coments

--- src/views/TimeLineEvent.vue
+++ src/views/TimeLineEvent.vue
@@ -6,6 +6,7 @@ const email = localStorage.getItem('email');
 export default {
   name: "App",
   computed:{
+    //funcion para agrupar los eventos por fecha
     groupedEvents() {
         const dataArray = Object.values(this.data);
       // Group the events by date
@@ -64,6 +65,7 @@ methods: {
     //Guardamos el id del usuario en el LocalStorage para luego utilizarlo en otras páginas
 
   },
+  //funcion para coger todos los eventos a los que te has apuntado y aun no han ocurrido
   async getEvents(){     
       
       const response = fetch (`http://puigmal.salle.url.edu/api/v2/users/${this.data2[0].id}/assistances/future`,{
